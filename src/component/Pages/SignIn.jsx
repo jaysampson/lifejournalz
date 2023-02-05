a5fdf6a53f7da2f94f5e0fc51ddf92b1f2edca39
Dayveed021 <boskey021@gmail.com>
2023-02-05 13:24:20 +0100

    lifejournalz

--- src/component/Pages/SignIn.jsx
+++ src/component/Pages/SignIn.jsx
@@ -57,59 +57,6 @@ const Login = () => {
   }, []);
   return (
     <>
-      {/* <div className='signin'>
-        <div className="signin_con">
-          <div className="signin_content">
-            <div className="title">
-              <p>Sign In</p>
-              <img src={Cancel} alt="X" />
-            </div>
-            <form action="" method='POST' onSubmit={handleSubmit}>
-              <p>
-                <label htmlFor="email">Email</label>
-              </p>
-              <input type="email"
-                required
-                placeholder='example@gmail.com'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <p>
-                <label htmlFor="password">Password</label>
-              </p>
-              <input type={passwordType ? "text" : "password"}
-                required
-                placeholder='********'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <div className="check_box">
-                <input type="checkbox" name="checkbox" onClick={togglePassword} id="checkbox" />
-                <label for="checkbox" className="checkbox">
-                  Show Password
-                </label>
-              </div>
-              <div className="signin_button">
-                <button>Sign In</button>
-              </div>
-              <img src={Or} alt="Or" />
-              <div className="usegoogle">
-                <button>Continue with google</button>
-              </div>
-              <div className="options">
-                <div className="no_account">
-                  <p>Don't have an account? <Link to="/signup" className='link'><span>Sign Up</span></Link></p>
-                </div>
-              </div>
-              <div className="forgot_p">
-                <Link to='/reset' className='link'><p>Forgot Password?</p></Link>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div> */}
-
-
       <div className="sigin" style={{ backgroundImage: `url(${cloud})` }}>
         <div className="container">
           <div className="logo">

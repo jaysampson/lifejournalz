783b57d317711b0ef1aa64a29be592fe75ca6e17
Dayveed021 <boskey021@gmail.com>
2023-02-16 22:49:54 +0100

    lifejournalz

--- src/component/Pages/Dashboard.jsx
+++ src/component/Pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "../../styles/dash.scss";
 import bell from "../../Images/bell.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUpFromBracket, faBriefcase, faClock, faFolderMinus, faHouse, faStar, faTag, faList, faGear, faArrowRightFromBracket, faHeart, faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUpFromBracket, faBriefcase, faClock, faFolderMinus, faHouse, faStar, faTag, faList, faGear, faArrowRightFromBracket, faHeart, faCalendar, faXmark } from '@fortawesome/free-solid-svg-icons';
 import plus from "../../Images/plus.png";
 import { useState } from 'react';
 import { Home } from './DashPages/Home';
@@ -16,12 +16,53 @@ import { Terms } from './DashPages/Terms';
 import { Calender } from './DashPages/Calender';
 import { Setting } from './DashPages/Setting';
 
+const MenuItem = ({ icon, label, isSelected, onClick }) => {
+    return (
+        <div className={isSelected ? 'icon2' : 'icon'} onClick={onClick}>
+            <FontAwesomeIcon icon={icon} />
+            <span>{label}</span>
+        </div>
+    );
+};
+
+const Menu = ({ selectedItem, setSelectedItem }) => {
+    const items = [
+        { id: 'dashboard', icon: faHouse, label: 'Dashboard' },
+        { id: 'categories', icon: faList, label: 'Categories' },
+        { id: 'favourites', icon: faStar, label: 'Favourites' },
+        { id: 'shared', icon: faArrowUpFromBracket, label: 'Shared', count: 24 },
+        { id: 'recent', icon: faClock, label: 'Recently added', count: 59 },
+        { id: 'storage', icon: faFolderMinus, label: 'Storage' },
+        { id: 'pricing', icon: faTag, label: 'Pricing', isNew: true },
+        { id: 'terms', icon: faBriefcase, label: 'Terms of Service' },
+    ];
+
+    return (
+        <div className="menu">
+            {items.map((item, index) => (
+                <MenuItem
+                    key={item.id}
+                    icon={item.icon}
+                    label={item.label}
+                    isSelected={selectedItem === index}
+                    onClick={() => {
+                        setSelectedItem(index);
+                    }}
+                />
+            ))}
+        </div>
+    );
+};
 
 const Dashboard = () => {
     const [selected, setSelected] = useState(0)
     const toggleMenu = (index) => {
         setSelected(index)
     }
+
+    const [selectedItem, setSelectedItem] = useState(0);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     const [activeComponent, setActiveComponent] = useState('Component1');
     return (
         <div>
@@ -30,11 +71,20 @@ const Dashboard = () => {
                     <div className='prof-pic'>
                         {/* <img src={user.picture}></img> */}
                     </div>
-                    <div className="notify">
-                        <div className="count">7</div>
-                        <img src={bell} alt="" />
+                    <div className="hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                        <svg viewBox="0 0 70 27" width="40">
+                            <rect width="40" height="6"></rect>
+                            <rect y="10" width="40" height="6"></rect>
+                            <rect y="20" width="40" height="6"></rect>
+                        </svg>
                     </div>
                 </div>
+                {isMenuOpen && (
+                    <div className="burg-con">
+                        <FontAwesomeIcon icon={faXmark} size={'2x'} style={{marginLeft:"90%"}} onClick={() => setIsMenuOpen(!isMenuOpen)} className="canc"/>
+                        <Menu selectedItem={selectedItem} setSelectedItem={setSelectedItem} />
+                    </div>
+                )}
                 <div className="dash-nav">
                     <div className="search">
                         <input type="text" placeholder='Search in Categories' className='nav-search' />
